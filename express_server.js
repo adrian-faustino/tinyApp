5860248fc464e9fd1da8c89a63034663bdcec211
Adrian Faustino <contact.adrianfaustino@gmail.com>
2020-03-19 03:08:58 +0000

    Installed cookie parser

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080;
 
@@ -8,6 +9,7 @@ const indexRouter = require('./routes/index');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', indexRouter);
+app.use(cookieParser());
 
 
 app.listen(PORT, () => {
