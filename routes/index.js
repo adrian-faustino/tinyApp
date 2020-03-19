60129788814803a751396f1299d0026b9863b8aa
Adrian Faustino <contact.adrianfaustino@gmail.com>
2020-03-19 05:56:22 +0000

    Functional version before adding user-registration

--- routes/index.js
+++ routes/index.js
@@ -37,13 +37,17 @@ router.get('/urls.json', (req, res) => {
 });
 
 router.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  let templateVars = {
+    username: req.cookies['username']
+  };
+  res.render('urls_new', templateVars);
 });
 
 router.get('/urls/:shortURL', (req, res) => {
   let templateVars = { 
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies['username']
   };
   res.render('urls_show', templateVars);
 });
