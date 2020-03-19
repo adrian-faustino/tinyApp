d0840b95dc3e538d96abfcf5123387a59be6b459
Adrian Faustino <contact.adrianfaustino@gmail.com>
2020-03-19 01:43:33 +0000

    Added a route to urls_show.ejs to display single URLs

--- routes/index.js
+++ routes/index.js
@@ -7,22 +7,29 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-// for '/'
+//========= for '/'
 router.get('/', (req, res) => {
   res.send('Hello!');
 });
 
-router.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+//========= for '/urls'
+router.get('/urls', (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
 
-// for '/urls'
-
-
-// for '/urls/~'
+//========= for '/urls/~'
 router.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
+});
+
+router.get('/urls/:shortURL', (req, res) => {
+  let templateVars = { 
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
+  res.render('urls_show', templateVars);
 });
 
 module.exports = router;
